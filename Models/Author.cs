7aa60ff4d8effe58479b41e6481038f5f3cbd54e
Oguzhan Kaya <oguzh@DESKTOP-UF0JMGP>
2023-03-24 23:08:57 +0300

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminBlog.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
